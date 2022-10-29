6e563b13a8b9bbd934329f7f66a3233b08b370ff
Ankur Here <avp@yourdomain.com>
2022-10-29 15:17:21 +0530

    initial first commit.

---
+++ coredemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  coredemo
+//
+//  Created by Kaushal Prajapati on 29/10/22.
+//
+
+import UIKit
+
+    class ViewController: UIViewController
+    {
+        override func viewDidLoad()
+        {
+            super.viewDidLoad()
+            print("demo")
+        }
+    }
+
